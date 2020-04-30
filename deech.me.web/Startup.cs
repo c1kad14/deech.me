134f510f8eb5c0a656ed50c97fc46292bb353d56
Yura Bobrykov <iurii.bobrykov@gmail.com>
2020-04-30 16:29:05 +1200

    client app clean up

--- deech.me.web/Startup.cs
+++ deech.me.web/Startup.cs
@@ -37,23 +37,12 @@ namespace deech.me.web
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
 
             app.UseSpa(spa =>
             {
